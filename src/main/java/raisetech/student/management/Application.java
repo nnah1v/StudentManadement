9262aa55871dc9d7f0d11862640289895ddeaa2f
Natsuka <nnah1v@gmail.com>
2025-12-11 10:32:01 +0900

    initial commit

---
+++ src/main/java/raisetech/student/management/Application.java
@@ -0,0 +1,20 @@
+package raisetech.student.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+  @GetMapping("/Natsu")
+  public String hello(){
+    return "Hello, Happy";
+  }
+
+}
+
